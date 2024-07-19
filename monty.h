2912a58994d87532e30994ec59d03569c4082cb4
“Bizcom56” <dornorjoshua56@gmail.com>
2024-07-19 11:07:33 -0700

    Prototypes

--- monty.h
+++ monty.h
@@ -10,6 +10,22 @@
 #include <ctype.h>
 
 /**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct stack_s
+{
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
+
+/**
  * struct globals - global structure to use in the functions
  * @lifo: is stack or queue
  * @cont: current line
@@ -19,7 +35,7 @@
  * @buffer: input text
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO ALX project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct globals
 {
@@ -31,49 +47,13 @@ typedef struct globals
 	char *buffer;
 } global_t;
 
-
-/*
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: Doubly linked list node structure
- *              for stack, queues, LIFO, FIFO ALX project
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
-
-
-
-/*get function*/
-void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
-
-/*imported functions*/
-int _sch(char *s, char c);
-char *_strtoky(char *s, char *d);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int _strcmp(char *s1, char *s2);
-
-/* doubly linked list functions */
-stack_t *add_dnodeint_end(stack_t **head, const int n);
-stack_t *add_dnodeint(stack_t **head, const int n);
-void free_dlistint(stack_t *head);
-
-
-
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
  * @f: function to handle the opcode
  *
  * Description: opcode and its function
- * for stack, queues, LIFO, FIFO ALX project
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct instruction_s
 {
@@ -82,7 +62,6 @@ typedef struct instruction_s
 } instruction_t;
 
 extern global_t vglo;
-
 
 /* opcode_instructuions*/
 void _push(stack_t **stack, unsigned int line_number);
@@ -103,7 +82,22 @@ void _pstr(stack_t **doubly, unsigned int cline);
 void _rotl(stack_t **doubly, unsigned int cline);
 void _rotr(stack_t **doubly, unsigned int cline);
 
+/*get function*/
+void (*get_opcodes(char *opc))(stack_t **stack, unsigned int line_number);
+
+/*imported functions*/
+int _sch(char *s, char c);
+char *_strtoky(char *s, char *d);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int _strcmp(char *s1, char *s2);
+
+/* doubly linked list functions */
+stack_t *add_dnodeint_end(stack_t **head, const int n);
+stack_t *add_dnodeint(stack_t **head, const int n);
+void free_dlistint(stack_t *head);
 
 /* main */
 void free_vglo(void);
+
 #endif
